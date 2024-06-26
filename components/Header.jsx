3bb389d805acb9906eece6b806b5f3f565a069b5
mstaali <imthetigerz@gmail.com>
2024-06-26 20:04:39 +0100

    setup_header && navbar_directions

---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            <span className="text-accent">M.</span>Staali
+          </h1>
+        </Link>
+        <div className="hidden xl:flex items-center gap-8">
+          <Navbar />
+          <Link href="/contact">
+            <Button>Contact me</Button>
+          </Link>
+        </div>
+        <div className="xl:hidden">mobile nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
